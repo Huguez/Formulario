5058846414dda89f734222a0f0dda5cc324c26c9
Huguez <chuguez@quantumbit.mx>
2019-11-22 16:56:19 -0700

    se hizo la aproximacion por codigo para validar un formulario en este template

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
+  
+  forma:FormGroup;
+  usuario:any = {
+    nombreCompleto: {
+      nombre: "Carlos",
+      apellido: "Huguez"
+    },
+    email: "carlos.huguez@gmail.com",
+    pasatiempos: [ "correr", "leer", "ver peliculas" ]
+  };
 
-  constructor() { }
+  constructor(){
+    this.forma = new FormGroup({
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl( '', [ Validators.required, Validators.minLength( 5 ) ] ),
+        'apellido': new FormControl( '', [ Validators.required, Validators.minLength( 5 ) ] )
+    }),
+      'email': new FormControl( '', [
+          Validators.required,
+          Validators.pattern( "[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" )
+        ] ),
+        'pasatiempos' : new FormArray( [ new FormControl("correr", Validators.required ) ] )
+    });
+  }
 
-  ngOnInit() {
+  ngOnInit(){}
+  
+  agregarPasatiempo(){
+    (<FormArray>this.forma.controls['pasatiempos']).push( new FormControl( '', Validators.required) );
+  }
+
+  guardarCambios(){
+    console.log( this.forma )
+    
+    //this.forma.setValue( this.usuario );
+    //this.forma.reset( this.usuario );
   }
 
 }
