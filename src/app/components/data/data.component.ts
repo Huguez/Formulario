626bffe8c7c40de420282ca9d2efaffaf1cbf2ee
Huguez <chuguez@quantumbit.mx>
2019-11-25 14:08:53 -0700

    se agrego validadores a personalizados, asincronos y detectores de cambios

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
-
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -8,8 +8,10 @@ import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
   styles: []
 })
 export class DataComponent {
-  
+  [x: string]: any;
+    
   forma:FormGroup;
+
   usuario:any = {
     nombreCompleto: {
       nombre: "Carlos",
@@ -23,14 +25,29 @@ export class DataComponent {
     this.forma = new FormGroup({
       'nombreCompleto': new FormGroup({
         'nombre': new FormControl( '', [ Validators.required, Validators.minLength( 5 ) ] ),
-        'apellido': new FormControl( '', [ Validators.required, Validators.minLength( 5 ) ] )
-    }),
+        'apellido': new FormControl( '', [ Validators.required, this.noHuguez ] )
+        }),
       'email': new FormControl( '', [
           Validators.required,
           Validators.pattern( "[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" )
-        ] ),
-        'pasatiempos' : new FormArray( [ new FormControl("correr", Validators.required ) ] )
+        ]),
+      'username': new FormControl( '', [ Validators.required], [this.existeUsuario] ),
+
+      'pasatiempos': new FormArray( [ new FormControl("correr", Validators.required ) ] ),
+      'password1': new FormControl('', Validators.required ),
+      'password2': new FormControl()
     });
+    
+    this.forma.controls['password2'].setValidators([ Validators.required, this.noIgual.bind( this.forma ) ] )
+
+    this.forma.controls['username'].valueChanges.subscribe( data => {
+      console.log( data );
+    });
+
+    this.forma.controls['username'].statusChanges.subscribe( data => {
+      console.log( data );
+    });
+
   }
 
   ngOnInit(){}
@@ -40,10 +57,37 @@ export class DataComponent {
   }
 
   guardarCambios(){
-    console.log( this.forma )
+    console.log( this.forma );
+  }
+
+  noHuguez( control: FormControl ): { [s:string]:boolean }{
+    if( control.value.toLowerCase() === "huguez" ){
+      return { noHuguez:true }
+    } 
+    return null;
+  }
+
+  noIgual( control: FormControl ): { [s:string]:boolean } {
     
-    //this.forma.setValue( this.usuario );
-    //this.forma.reset( this.usuario );
+    if( control.value !== this.controls['password1'].value ){
+      return {  noiguales: true }
+    }
+
+    return null; 
+  }
+
+  existeUsuario( control: FormControl  ): Promise<any> | Observable<any> {
+      
+     let promesa = new Promise( ( resolve, reject ) => {
+       setTimeout( ()=> {
+        if( control.value === 'hgz' ){
+          resolve( { existe: true } );
+        }else resolve( null )
+        
+      }, 1000 )
+    });
+
+    return promesa;
   }
 
 }
